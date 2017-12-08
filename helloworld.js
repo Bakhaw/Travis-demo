fa1a3c978898a7987e0d54723052cb7d26795c40
Faïssal <faissal.hattou@gmail.com>
2017-12-08 15:30:16 +0100

    ADD: first test

---
+++ helloworld.js
@@ -0,0 +1,6 @@
+function helloWorld() {
+    // Write your code here
+    // GOOD LUCK & HAVE FUN :D
+}
+
+module.exports = helloWorld;
---
+++ test/test.js
@@ -0,0 +1,10 @@
+const chai = require("chai");
+const expect = require("chai").expect;
+const HelloWorld = require("../helloworld");
+
+describe("The famous Hello World test", function() {
+  it("Should return Hello World", function() {
+    const helloWorld = new HelloWorld();
+    expect(helloWorld.coucou).to.equal("Hello World;");
+  });
+});
